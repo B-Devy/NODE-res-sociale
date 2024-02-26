4adbd534646d876d7aa4114c7375265c291f69ec
B-Devy <bertrandcafolla@hotmail.com>
2024-02-26 20:49:37 +0100

    final

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,32 @@ const router = require('express').Router();
 const User = require('../models/User');
 
 
-// Create new Post
+// Affiche followings
 
 router.get("/friends/:userId", async (req, res) => {
     try {
         const user = await User.findById(req.params.userId);
         const friends = await Promise.all(user.followings.map((fri_id) => {
+            return User.findById(fri_id);
+        }));
+        
+        let friendList = [];
+        friends.map((friend) => {
+            const { _id, username, profilePicture } = friend;
+            friendList.push({ _id, username, profilePicture })
+        })
+        res.status(200).json(friendList)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+//  Affiche Followed
+
+router.get("/befriendswith/:userId", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.userId);
+        const friends = await Promise.all(user.followers.map((fri_id) => {
             return User.findById(fri_id);
         }));
         
